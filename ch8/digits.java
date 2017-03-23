f84092182d9a96c7e11dd6aca30531a56a182e71
mlsanborn <MLSanborn@student.naperville203.org>
2017-03-23 10:30:12 -0500

    3-23-17

---
+++ ch8/digits.java
@@ -0,0 +1,19 @@
+
+
+/**
+ * Write a description of class digits here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class digits
+{
+    /**
+     * Default constructor for objects of class digits
+     */
+    public digits()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+}
---
+++ ch8/palindrome.java
@@ -0,0 +1,36 @@
+
+
+/**
+ * Write a description of class palindrome here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class palindrome
+{
+    
+    public palindrome(String word)
+    {
+        String word = w.replace(" ", "");
+        
+        
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
